4a1082b2c46fd6f4f6d2895b7ce01fb3933af3be
brazhenriqueti <brazhenriqueti@outlook.com>
2021-12-23 12:33:40 -0300

    cambios en el legales, rrhh, login, key

--- src/Keys.js
+++ src/Keys.js
@@ -1,6 +1,6 @@
 const Entidad = "MegaFront";
 const GoogleMapKey = "AIzaSyDwKzicjMvfVsf9SbEAZn9N7i4CxX6YO1I";
-const RecaptchaKey = "6LdeSNUbAAAAAFGu6eZhNDV75LCvbD5Zbf7zaudU";
+const RecaptchaKey = "6LeALiAcAAAAABv5WAeRHUmaf5GfqNSBYn8wTKON";
 const UrlApiDynamics = "https://sgroc365api.azurewebsites.net/api/";
 
 // const Entidad = "HWapplications";
