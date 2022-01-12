a2455cdf726e06b5830a4b361757919251e94ee3
Your Name <hbraz@hwapplications>
2022-01-12 14:26:30 -0300

    deriva y area a escalar form casos

--- src/Components/Casos.jsx
+++ src/Components/Casos.jsx
@@ -300,6 +300,7 @@ const Casos = (props) => {
     }
 
     if (asuntoSeleccionar === "") {
+      cosole.log(asuntoSeleccionar)
       setMensaje("Asunto es requerido!");
       setError(true);
       setShow(true);
